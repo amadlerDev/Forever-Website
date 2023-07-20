60bdf5f62454007ae9e2d7177445472ba1333cd7
AAdler <amadler.dev@gmail.com>
2023-07-21 01:03:11 +0300

    add pages

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
+/**
+ * Render App
+ * From this file all the pages runs
+ */
+
+
+// local import
 import Menu from './Menu.js';
+import Home from './Home.js';
+import Shop from './Shop.js';
+import Contact from './Contact.js';
 import './css/App.css';
+
+// dependencies
 import { BrowserRouter, Routes, Route } from "react-router-dom"; 
 
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <BrowserRouter>
-          <Routes>
-              <Route path="/" element={<Menu />}>
-                  <Route path="*" />
-              </Route>
-          </Routes>
-        </BrowserRouter>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header"> 
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<Menu />}>
+                            <Route path="/" element={<Home />} />
+                            <Route path="shop" element={<Shop />} />
+                            <Route path="contact" element={<Contact />} />
+                            <Route path="*" />
+                        </Route>
+                    </Routes>
+                </BrowserRouter>
+            </header>
+        </div>
+    );
 }
 
 export default App;

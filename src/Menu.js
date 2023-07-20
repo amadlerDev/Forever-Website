77a77e21d60b84a9e2aba8900c3bdd5644f6e872
AAdler <amadler.dev@gmail.com>
2023-07-21 00:42:42 +0300

    first init

---
+++ src/Menu.js
@@ -0,0 +1,29 @@
+import { Outlet, Link } from "react-router-dom";
+// import './Menu.css';
+
+
+
+const Menu = () => {
+    return (
+        <>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/shop">Shop</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+  
+        <Outlet />
+      </>
+    )
+
+}
+
+export default Menu;

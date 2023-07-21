18917ff98830c746915ebf8c130c030dfa1a4b6e
AAdler <amadler.dev@gmail.com>
2023-07-21 12:10:33 +0300

    add products to shop page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,18 @@
 
 
 const Contact = () => {
-    return <h4>Contact</h4>;
+    return (
+        <div className="contact">
+            <h4>Contact</h4>        
+
+            <form>
+                <h5>Contact Me!</h5>
+                <label></label>
+                <input></input>
+                <button></button>
+            </form>        
+        </div>
+    )
 };
   
 export default Contact;
--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,7 +1,40 @@
+import productImg from '../assets/background-home.jpeg';
 
+
+const products = [
+    <Product />, 
+    <Product />, 
+    <Product />, 
+    <Product />, 
+    <Product />, 
+    <Product />, 
+    <Product />, 
+    <Product />, 
+    <Product />, 
+    <Product />, 
+    <Product />, 
+    <Product />, 
+    <Product />, 
+    <Product />, 
+    <Product />, 
+    <Product />, 
+]
+
+function Product() {
+    return (
+        <a className="product" href="#">
+            <img src="http://localhost:3000/static/media/background-home.3fc549e20848875296aa.jpeg" alt="product" />
+        </a>
+    )
+};
 
 const Shop = () => {
-    return <h4>Shop</h4>;
+    return (
+        <div className="shop">
+            <h4>Shop</h4>
+            <div className="products">{products}</div>
+        </div>
+    )
 };
   
 export default Shop;
